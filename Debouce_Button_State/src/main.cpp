4a853e10b2ff5796d1a7a3504e43fb1e704c3776
Arnat Ngawsuwan <annsw.work@gmail.com>
2023-01-29 20:38:05 +0700

    add a state and play with led

--- Debouce_Button_State/src/main.cpp
+++ Debouce_Button_State/src/main.cpp
@@ -1,12 +1,16 @@
 #include<Arduino.h>
-#define BUTTON_PIN 2
+#define BUTTON_PIN 12
+#define LED1 13
+#define LED2 14
 
 unsigned long currentMillis, previousMillis;
 int currentButtonState, lastButtonState, lastDebouncedButtonState;
+int state = 0;
 void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  pinMode(3, OUTPUT);
-  Serial.begin(9600);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  Serial.begin(115200);
   currentMillis = millis();
   previousMillis = currentMillis;
 }
@@ -20,12 +24,35 @@ void loop() {
   }
   if((currentMillis - previousMillis) > 50 ){
     if (lastButtonState == HIGH && currentButtonState == LOW) {
+      state++;
+      if (state > 3){
+        state = 0;
+      }
       Serial.println("Button Pressed");
+      Serial.println(state);
     }
     else if(lastButtonState == LOW && currentButtonState == HIGH){
-      Serial.println("Button Released"); 
+      Serial.println("Button Released");
     }
     lastButtonState = currentButtonState;
   }
-  
+
+  switch (state) {
+    case 0:
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW);
+      break;
+    case 1:
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      break;
+    case 2:
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, HIGH);
+      break;
+    case 3:
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH);
+      break;
+  }
 }
